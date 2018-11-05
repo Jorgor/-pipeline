5db7574f06dbb2a56565412b4ed8e4846653c1ae
2016caicai <1196363729@qq.com>
2018-11-05 09:12:59 +0800

    complate map

--- wap/src/app/module/work-order/work-map/work-map.component.ts
+++ wap/src/app/module/work-order/work-map/work-map.component.ts
@@ -41,22 +41,31 @@ export class WorkMapComponent implements OnInit {
     this.settingsService.createTag('assets/js/amapui/main.js', 'amapui', () => this.setMap());
   }
   setMap() {
-    console.log(this.detail);
+    // console.log(this.detail);
     this.addSatellite();
     this.addController();
-    this.detail.trail ? this.setPolyline(this.detail.trail) :
-    this.goToLocation([106.424402398004, 29.821534016928])
+    if (this.detail.trail && this.detail.patrolPlan) {
+      this.setPolyline(this.detail.trail);
+    }
+    if (this.detail.alarm) {
+      const { object } = this.detail.alarm.target;
+      if (object && object.coordinate) {
+        this.goToLocation([object.coordinate.longitude, object.coordinate.latitude]);
+      } else {
+        this.setPolyline([].concat(object.endPoint.coordinate, object.startPoint.coordinate));
+      }
+    }
   }
   //  画轨迹
   setPolyline(trail) {
-    const finalPath = trail.reduce((n,a) => {
-        n.push([a.longitude, a.latitude])
-        return n
-    }, [])
+    const finalPath = trail.reduce((n, a) => {
+        n.push([a.longitude, a.latitude]);
+        return n;
+    }, []);
     this.map.setCenter(finalPath[0]);
     const polyline = new this.AMap.Polyline({
         showDir: true,
-        strokeColor: 'ligthgray', // 线条颜色
+        strokeColor: 'red', // 线条颜色
         lineJoin: 'round',
         strokeWeight: 3,
         borderWeight: 1,
@@ -67,10 +76,10 @@ export class WorkMapComponent implements OnInit {
     // 缩放地图到合适的视野级别
     this.map.setFitView([ polyline ])
   }
-  //定位到指定点，并跳动标记
+  // 定位到指定点，并跳动标记
   goToLocation(position) {
     this.map.setCenter(position);
-    let marker = new this.AMap.Marker({
+    const marker = new this.AMap.Marker({
         icon: new this.AMap.Icon({
             image: '/assets/img/blue_water.png',
             imageSize: new this.AMap.Size(25, 30),
