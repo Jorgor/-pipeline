1a16a48d59ec3e41c041992d57637c9ab434a8cc
2016caicai <1196363729@qq.com>
2018-11-08 17:41:49 +0800

    可地地图修改

--- wap/src/app/module/home/home.component.ts
+++ wap/src/app/module/home/home.component.ts
@@ -4,7 +4,8 @@ import { WorkOrderService } from '../work-order/work-order.service';
 
 import {Router} from '@angular/router';
 declare const layer: any;
-
+declare var AMap: any;
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -53,9 +54,9 @@ export class HomeComponent implements OnInit {
   report() {
     const {id} = this.resloveDate;
     if (navigator.geolocation) {
+      this.reportLoading = layer.load();
       navigator.geolocation.getCurrentPosition( (position) => {
         const {longitude, latitude} = position.coords;
-        this.reportLoading = layer.load();
         this.workServer.postPoint(id, {longitude, latitude}).then(res => {
             layer.close(this.reportLoading);
             this.reportLoading = false;
@@ -64,12 +65,62 @@ export class HomeComponent implements OnInit {
         }, (e) => {
          // const msg = e.code;
          const dd = e.message;
-         layer.msg(dd);
+         this.initmap(id);
       });
     }else {
       layer.msg('请打开手机GPS')
+      this.initmap(id);
     }
     // this.workServer.postPoint();
+  }
+  initmap(id) {
+    const script = document.createElement('script');
+    const option = {
+      opt: {
+          resizeEnable: true,
+          zoom: 17
+      }, key: 'f3cfcdc7262b6db0f223b3b9d57b120b'
+    };
+    if ($('#reportMap').length) {
+     return this.getReport(option, id);
+    }
+    script.id = 'reportMap';
+    script.type = 'text/javascript';
+    script.src = 'http://webapi.amap.com/maps?v=1.4.5&key='+option.key+'&plugin=AMap.DistrictSearch,AMap.Autocomplete,AMap.PlaceSearch,AMap.ToolBar';
+    script.className = 'map';
+    script.onload = () => {
+      this.getReport(option,id);
+    }
+    document.body.appendChild(script);
+  }
+  getReport(option, id) {
+    const map = new AMap.Map('hide-map', option.opt);
+    map.plugin('AMap.Geolocation', () => {
+      var geolocation = new AMap.Geolocation({
+        // 是否使用高精度定位，默认：true
+        enableHighAccuracy: true,
+        // 设置定位超时时间，默认：无穷大
+        timeout: 10000,
+        // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
+        buttonOffset: new AMap.Pixel(10, 20),
+        //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        zoomToAccuracy: true,     
+        //  定位按钮的排放位置,  RB表示右下
+        buttonPosition: 'RB'
+      });
+      geolocation.getCurrentPosition();
+      AMap.event.addListener(geolocation, 'complete', (data) => {
+        const {lng: longitude, lat: latitude} = data.position;
+          this.workServer.postPoint(id, {longitude, latitude}).then(res => {
+            layer.close(this.reportLoading);
+            this.reportLoading = false;
+            layer.msg(res.msg);
+          });
+      });
+      AMap.event.addListener(geolocation, 'error', (data) => {
+        layer.msg(data.message);
+      });
+    });
   }
   btnEmit(event) {
     if (!!event) { this[event](); }
--- wap/src/app/module/work-order/work-detail/work-detail.component.ts
+++ wap/src/app/module/work-order/work-detail/work-detail.component.ts
@@ -6,6 +6,8 @@ import { WorkOrderService } from '../work-order.service';
 import {WEB_URL_PREFIX} from '../../../../assets/server/http-link.data';
 
 declare const layer: any;
+declare var AMap: any;
+declare var $: any;
 
 @Component({
   selector: 'app-work-detail',
@@ -70,9 +72,9 @@ export class WorkDetailComponent implements OnInit {
   report() {
     const {id} = this.detail;
     if (navigator.geolocation) {
+      this.reportLoading = layer.load();
       navigator.geolocation.getCurrentPosition( (position) => {
         const {longitude, latitude} = position.coords;
-        this.reportLoading = layer.load();
         this.serve.postPoint(id, {longitude, latitude}).then(res => {
             layer.close(this.reportLoading);
             this.reportLoading = false;
@@ -81,11 +83,61 @@ export class WorkDetailComponent implements OnInit {
         }, (e) => {
          // const msg = e.code;
          const dd = e.message;
-         layer.msg(dd);
+         this.initmap(id);
       });
     }else {
       layer.msg('请打开手机GPS')
+      this.initmap(id);
     }
+  }
+  initmap(id) {
+    const script = document.createElement('script');
+    const option = {
+      opt: {
+          resizeEnable: true,
+          zoom: 17
+      }, key: 'f3cfcdc7262b6db0f223b3b9d57b120b'
+    };
+    if ($('#reportMap').length) {
+     return this.getReport(option, id);
+    }
+    script.id = 'reportMap';
+    script.type = 'text/javascript';
+    script.src = 'http://webapi.amap.com/maps?v=1.4.5&key='+option.key+'&plugin=AMap.DistrictSearch,AMap.Autocomplete,AMap.PlaceSearch,AMap.ToolBar';
+    script.className = 'map';
+    script.onload = () => {
+      this.getReport(option,id);
+    }
+    document.body.appendChild(script);
+  }
+  getReport(option, id) {
+    const map = new AMap.Map('hide-map', option.opt);
+    map.plugin('AMap.Geolocation', () => {
+      var geolocation = new AMap.Geolocation({
+        // 是否使用高精度定位，默认：true
+        enableHighAccuracy: true,
+        // 设置定位超时时间，默认：无穷大
+        timeout: 10000,
+        // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
+        buttonOffset: new AMap.Pixel(10, 20),
+        //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        zoomToAccuracy: true,     
+        //  定位按钮的排放位置,  RB表示右下
+        buttonPosition: 'RB'
+      });
+      geolocation.getCurrentPosition();
+      AMap.event.addListener(geolocation, 'complete', (data) => {
+        const {lng: longitude, lat: latitude} = data.position;
+          this.serve.postPoint(id, {longitude, latitude}).then(res => {
+            layer.close(this.reportLoading);
+            this.reportLoading = false;
+            layer.msg(res.msg);
+          });
+      });
+      AMap.event.addListener(geolocation, 'error', (data) => {
+        layer.msg(data.message);
+      });
+    });
   }
   feedback() {
     const {id} = this.detail;
